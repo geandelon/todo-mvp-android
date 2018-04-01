4df91ae961ec605b47949e98becda44b6d693252
cytst <cytsts@gmail.com>
2018-04-01 18:23:33 -0300

    Adicionado alguns tratamentos para o evento e redução de codigo repetido na exibição de log no contador

--- app/src/main/java/digaoperadora/delon/br/teste/base31_03/SplashPresenter.kt
+++ app/src/main/java/digaoperadora/delon/br/teste/base31_03/SplashPresenter.kt
@@ -8,19 +8,23 @@ import android.util.Log
 class SplashPresenter : MvpPresenter() {
     override val TAG: String = SplashPresenter::class.java.simpleName
 
-    lateinit var evento : (Int)->Unit
+    var evento: (Int) -> Unit = {
+        Log.d(TAG, "Evento do Presenter sem implementação na visão, recebeu um valor:  ${it}, como parametro")
+    }
 
-    public fun setOnContadorChange(evento:(Int)->Unit ): Unit {
+    public fun setOnContadorChange(evento: (Int) -> Unit): Unit {
         this.evento = evento
     }
 
 
-
     var contador: Int = 0
-    set(value) {
-        evento(value)
-        field = value
-    }
+        set(value) {
+            evento?.let {
+                it(value)
+            }
+            field = value
+            Log.d(TAG, value.toString())
+        }
 
 
     fun abc() {
@@ -30,41 +34,30 @@ class SplashPresenter : MvpPresenter() {
     fun fdf4() {
         Log.d(TAG, "ON_CREATE")
         contador++
-        Log.d(TAG, contador.toString())
-
     }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_START)
     fun fdf() {
         Log.d(TAG, "ON_START")
         contador++
-        Log.d(TAG, contador.toString())
-
     }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
     fun fdf2() {
         Log.d(TAG, "ON_RESUME")
         contador++
-        Log.d(TAG, contador.toString())
-
     }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
     fun fdf3() {
         Log.d(TAG, "ON_PAUSE")
         contador++
-        Log.d(TAG, contador.toString())
-
     }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
     fun fdf5() {
         Log.d(TAG, "ON_STOP")
         contador++
-
-        Log.d(TAG, contador.toString())
-
     }
 
 }
